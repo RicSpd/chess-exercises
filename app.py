e0d233f60dd886ecfc29bd88823c181e951f8ad1
Riccardo Spadini <rspadini@bitbang.com>
2022-04-20 14:21:49 +0200

    added start button for exercises; added favicons in the footer

--- app.py
+++ app.py
@@ -6,9 +6,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
+    """
+    Homepage
+    """
     return render_template("homepage.html")
 
 
 @app.route("/exercises")
 def exercises():
+    """
+    Page with the exercises that will return the FEN
+    """
     return render_template("index.html")
