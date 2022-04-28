263a8f862d64709f9b9d125819095e92dbabeb1e
Riccardo Spadini <rspadini@bitbang.com>
2022-04-28 15:48:49 +0200

    added button that receives POST request

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template
+import json
+from flask import Flask, render_template, request, Response
+from chess_exercises.fen import ChessBoard
 
 
 app = Flask(__name__)
 
 
-@app.route("/homepage")
+@app.route("/homepage", methods=['GET'])
 def home():
     """
     Homepage
@@ -12,9 +14,29 @@ def home():
     return render_template("homepage.html")
 
 
-@app.route("/exercises")
+@app.route("/exercises", methods=['GET', 'POST'])
 def exercises():
     """
-    Page with the exercises that will return the FEN
+    Page with the exercises that will return the FEN and the chessboard image
     """
+    if request.method == 'POST':
+        fen = 'EUREKA!'
+        # randomized = request.data.get('randomized')
+        # extra_populated = request.data.get('extra_populated')
+        return render_template("exercises.html", fen=fen)
+
     return render_template("exercises.html")
+
+    # data = None
+    # if request.content_type == "application/json":
+    #     data = json.loads(request.data)
+    # else:
+    #     return Response(response="This predictor only supports JSON data.", status=415, mimetype="text/plain")
+
+    # # # Do the prediction
+    # # predictions = ScoringService(data).predict()
+
+    # # Convert from json to string
+    # result = json.dumps({'bella': 'ciao'})
+
+    # return Response(response=result, status=200, mimetype="application/json")
--- static/js/exercises.js
+++ static/js/exercises.js
@@ -10,4 +10,76 @@ function showSelectPieces() {
     } else {
         card.style.display = "none";
     }
-  }
+}
+
+
+
+function postData(url, payLoad) {
+    var jsonData = JSON.stringify(payLoad);
+    return $.ajax({
+        type: "POST",
+        crossDomain: true,
+        dataType: "json",
+        contentType: "application/json",
+        data: jsonData,
+        url: url
+    });
+}
+
+
+
+jQuery(window).on("load", function() {
+
+    document.getElementById("go").addEventListener("click", function() {
+
+        randomized = document.getElementById("randomized").checked
+        extra_populated = document.getElementById("extra-populated").checked
+        equal_material = document.getElementById("equal-material").checked
+
+        white_pawns = document.getElementById("white-pawns").options.value
+
+        payload = {
+            "randomized": randomized,
+            "extra_populated": extra_populated,
+            "equal_material": equal_material,
+            "select_pieces": {
+                "white_pawns": white_pawns,
+            }
+        }
+        
+        result = postData("/exercises", payload)
+
+        // result.done(function(response) {
+
+        //     // create a new div element containing the new chart
+        //     placeholder = document.createElement('div');
+        //     placeholder.innerHTML = response['div'];
+        //     divsBokehPlot = placeholder.children;
+
+        //     // get span element to remove: necessary to remove old plots
+        //     document.getElementById("spanGraph").remove();
+
+        //     // appending div element
+        //     newSpan = document.createElement('span')
+        //     newSpan.setAttribute('id', 'spanGraph')
+        //     newSpan.append(divsBokehPlot[0])
+        //     document.getElementById("graph").append(newSpan)
+            
+        //     // creating script DOM element
+        //     placeholder2 = document.createElement('div');
+        //     placeholder2.innerHTML = response['script'];
+        //     script_bokeh = placeholder2.children;
+
+        //     // executing Javascript script
+        //     jQuery.globalEval(script_bokeh[0].text) 
+
+        //     console.log("Get data done!")
+
+        // }).fail(function() {
+        //     console.log("POST Ajax call failed.")
+        //     document.getElementById("graph_text").innerHTML = "--- Failed ---"
+        // })
+
+    });
+
+});
